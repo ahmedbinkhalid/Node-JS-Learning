dcc1b5dcac1ea4dfb7e222e0004ffebbfc2567aa
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-20 15:05:57 +0500

    grade

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -1,7 +1,15 @@
-console.log("Let's Do it Ahmed")
-let num = prompt("Enter number here")
-if (num % 5 === 0){
-    console.log("It is a multiple of 5")
-} else {
-    console.log("Not multiple of 5")
+// console.log("Let's Do it Ahmed")
+// let num = prompt("Enter number here")
+// if (num % 5 === 0){
+//     console.log("It is a multiple of 5")
+// } else {
+//     console.log("Not multiple of 5")
+// }
+let num = prompt("Enter you marks here")
+if (num >=80){
+    alert("your grade is A")
+}else if (70>= num <=779){
+    alert("your grade is B")
+}else{
+    alert("You are avaerage")
 }
