e2cf713fc570c3958eb368a4709605941f979972
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-20 16:38:25 +0500

    For-of Loop

--- JavaScript/first.js
+++ JavaScript/first.js
@@ -5,11 +5,20 @@
 // } else {
 //     console.log("Not multiple of 5")
 // }
-let num = prompt("Enter you marks here")
-if (num >=80){
-    alert("your grade is A")
-}else if (70>= num <=779){
-    alert("your grade is B")
-}else{
-    alert("You are avaerage")
+// let num = prompt("Enter you marks here")
+// if (num >=80){
+//     alert("your grade is A")
+// }else if (70>= num <=779){
+//     alert("your grade is B")
+// }else{
+//     alert("You are avaerage")
+// }
+
+// For-of Loop
+let fulName = "Ahmed Bin Khalid"
+let size = 0;
+for(let val of fulName){
+    console.log("val:", val);
+    size++;
 }
+alert(size)
