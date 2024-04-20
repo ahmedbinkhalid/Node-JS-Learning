5e63a91dc8730731d2148ab314a86f4f18f91e0b
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-20 14:59:37 +0500

    basics

---
+++ JavaScript/first.js
@@ -0,0 +1,7 @@
+console.log("Let's Do it Ahmed")
+let num = prompt("Enter number here")
+if (num % 5 === 0){
+    console.log("It is a multiple of 5")
+} else {
+    console.log("Not multiple of 5")
+}
