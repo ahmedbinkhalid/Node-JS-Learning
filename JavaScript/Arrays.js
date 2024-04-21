10ab332d8e551103eeb62fd42d83f65034615c66
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-21 17:42:07 +0500

    // Change array after applying 10% off

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -1,10 +1,21 @@
-// Practicing Arrays
-// Find Avaerage marks of class 
+// // Practicing Arrays
+// // Find Avaerage marks of class 
 
-let marks = [34,53,3,3,32,42,6,86,97,57,56,43];
-let sum= 0;
-for(let mark of marks){
-    sum += mark;
+// let marks = [34,53,3,3,32,42,6,86,97,57,56,43];
+// let sum= 0;
+// for(let mark of marks){
+//     sum += mark;
+// }
+// let avg= sum / marks.length;
+// console.log(`Average Marks are ${avg}`);
+
+// Change array after applying 10% off
+let prices = [788,900,345,600,400];
+let id = 0;
+for(let i of prices) {
+    let offer = i/10;
+    prices[id] =  i - offer;
+    console.log(prices[id]);
+    id++;
 }
-let avg= sum / marks.length;
-console.log(`Average Marks are ${avg}`);
+console.log('After Applying Discount: ',prices);
