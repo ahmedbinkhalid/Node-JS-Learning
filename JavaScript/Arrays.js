88786ac5d113b762d3baca8f2c93e5da42a9b52b
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-21 17:53:06 +0500

    // Practicing Array (Add,remove,replace)

--- JavaScript/Arrays.js
+++ JavaScript/Arrays.js
@@ -9,13 +9,21 @@
 // let avg= sum / marks.length;
 // console.log(`Average Marks are ${avg}`);
 
-// Change array after applying 10% off
-let prices = [788,900,345,600,400];
-let id = 0;
-for(let i of prices) {
-    let offer = i/10;
-    prices[id] =  i - offer;
-    console.log(prices[id]);
-    id++;
-}
-console.log('After Applying Discount: ',prices);
+// // Change array after applying 10% off
+// let prices = [788,900,345,600,400];
+// let id = 0;
+// for(let i of prices) {
+//     let offer = i/10;
+//     prices[id] =  i - offer;
+//     console.log(prices[id]);
+//     id++;
+// }
+// console.log('After Applying Discount: ',prices);
+
+// Practicing Array (Add,remove,replace)
+
+let companies = ["Facebook","Google","Microsoft"];
+companies.shift();
+companies.splice(1,1,"Apple");
+companies.push("samsung");
+console.log(companies);
