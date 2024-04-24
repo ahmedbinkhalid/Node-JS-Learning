2fd874d775ef00163dd32b1067d7c41fa5c4a858
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-04-25 01:02:41 +0500

    ForEach Loop in Arrays combined with Arrow Function

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -35,19 +35,26 @@
 // }
 // myfunction(prompt("Enter  any string"));
 
-// Practice Question -> Extract Vowels from String Using Arrow Functions
-let count = 0;
-const vowels = (str) => {
-    for(vw of str){
-        if(vw == "a" || vw == "e" || vw == "i" || vw == "o" || vw == "u" ){
-            console.log(vw)
-            count++;
-        }
+// // Practice Question -> Extract Vowels from String Using Arrow Functions
+// let count = 0;
+// const vowels = (str) => {
+//     for(vw of str){
+//         if(vw == "a" || vw == "e" || vw == "i" || vw == "o" || vw == "u" ){
+//             console.log(vw)
+//             count++;
+//         }
         
-    }
-    console.log("Total Number of Vowels are:", count)
-    console.log("The String You Entered Was:", str)
-}
- vowels(prompt("Enter  any string"));
+//     }
+//     console.log("Total Number of Vowels are:", count)
+//     console.log("The String You Entered Was:", str)
+// }
+//  vowels(prompt("Enter  any string"));
+
+// ForEach Loop in Arrays combined with Arrow Funtion
+
+let arr = ["ahmed", "Ali", "Hassan"]
+arr.forEach((val) => {
+    console.log(val.toLocaleUpperCase())
+});
     
 
