6803bb567514a9fab4af24bc5413803c91866867
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-16 01:33:50 +0500

    JavaScript Refresher for Node

---
+++ NODE-FIRST-APP/Javascript_Refresher/play.js
@@ -0,0 +1,11 @@
+var userNamed = 'Ahmed';
+var age = 22;
+var hasHobbies = true;
+function sumerizeUser(userName, userAge, userHobby) {
+    return (
+        "Name is " + userName + ", Age is " + userAge + ", Has hobbies " + userHobby
+    );
+
+}
+
+console.log(sumerizeUser(userNamed, age, hasHobbies))
