ff0f14baad7b047adf685f4f07e08c72b787b87c
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-20 00:25:08 +0500

    Arrow/function within objects

--- NODE-FIRST-APP/Javascript_Refresher/play.js
+++ NODE-FIRST-APP/Javascript_Refresher/play.js
@@ -1,20 +1,34 @@
-const userNamed = 'Ahmed';
-let age = 22;
-const hasHobbies = true;
-const sumerizeUser = (userName, userAge, userHobby) => {
-    return (
-        "Name is " + userName + ", Age is " + userAge + ", Has hobbies " + userHobby
-    );
+// // Arrow function
 
-}
-const add = (a,b) =>{
-return a+b 
+// const userNamed = 'Ahmed';
+// let age = 22;
+// const hasHobbies = true;
+// const sumerizeUser = (userName, userAge, userHobby) => {
+//     return (
+//         "Name is " + userName + ", Age is " + userAge + ", Has hobbies " + userHobby
+//     );
+
+// }
+// const add = (a,b) =>{
+// return a+b 
+// };
+
+// const addone = a => a +1 ;
+// const addrandom = () => 1+2;
+
+// console.log(add(1, 2))
+// console.log(addone(1))
+// console.log(addrandom())
+// console.log(sumerizeUser(userNamed, age, hasHobbies))
+
+// Arrow/function within objects
+
+const person = {
+    name: 'max',
+    age: 19,
+    greet(){
+        console.log("Hello " + this.name);
+    }
 };
 
-const addone = a => a +1 ;
-const addrandom = () => 1+2;
-
-console.log(add(1, 2))
-console.log(addone(1))
-console.log(addrandom())
-console.log(sumerizeUser(userNamed, age, hasHobbies))
+person.greet()
