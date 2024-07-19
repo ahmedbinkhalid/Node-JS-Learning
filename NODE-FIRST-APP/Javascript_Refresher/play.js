a556870f1ea2fec166626ce925e090b1cd0879bf
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-20 00:15:01 +0500

    tried arrow functions with different parameters

--- NODE-FIRST-APP/Javascript_Refresher/play.js
+++ NODE-FIRST-APP/Javascript_Refresher/play.js
@@ -1,11 +1,20 @@
 const userNamed = 'Ahmed';
 let age = 22;
 const hasHobbies = true;
-function sumerizeUser(userName, userAge, userHobby) {
+const sumerizeUser = (userName, userAge, userHobby) => {
     return (
         "Name is " + userName + ", Age is " + userAge + ", Has hobbies " + userHobby
     );
 
 }
+const add = (a,b) =>{
+return a+b 
+};
 
+const addone = a => a +1 ;
+const addrandom = () => 1+2;
+
+console.log(add(1, 2))
+console.log(addone(1))
+console.log(addrandom())
 console.log(sumerizeUser(userNamed, age, hasHobbies))
