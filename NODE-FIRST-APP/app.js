ed5e018ec9251e6c043b7fcfe8d1526f30c60f5e
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-21 22:43:26 +0500

    adding middleware

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -2,7 +2,16 @@ const http = require('http');
 
 const express = require("express");
 
-const app = express;
+const app = express();
+
+app.use((req, res, next)=>{
+    console.log("middleware 1");
+    next();
+});
+
+app.use((req, res, next)=>{
+    console.log("Middleware 2");
+});
 
 const server = http.createServer(app);
 
