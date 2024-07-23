21f3aa38f5338e41c65934eaa71d79237027d4e4
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-23 23:31:01 +0500

    Serving files statically

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -14,11 +14,13 @@ const path = require('path');
 
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use(express.static(path.join(__dirname,'public')));
+
 app.use('/admin',adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next)=>{
-    res.status(404).sendFile(path.join(__dirname, 'views', 'noPage.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
