4495c34f785a485c46f1120e4339df81998b45d8
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-25 02:07:46 +0500

    Sharing data across requests & users

--- NODE-FIRST-APP/Routes/shop.js
+++ NODE-FIRST-APP/Routes/shop.js
@@ -3,11 +3,16 @@ const express = require('express');
 const rootDir = require('../util/path')
 
 const path = require('path');
+const { product } = require('./admin');
+
+const adminData = require('./admin');
 
 const router = express.Router();
 
 
 router.get('/', (req, res, next)=>{
+    console.log(adminData.product);
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+    
 });
 module.exports = router;
--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRouter = require('./Routes/admin');
+const adminData = require('./Routes/admin');
 
 const shopRouter = require('./Routes/shop');
 
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.use('/admin',adminRouter);
+app.use('/admin',adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next)=>{
