99865dedb51199015af5f54eeabf73f2478d864d
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-27 01:19:24 +0500

    pug ejs handlebars

--- NODE-FIRST-APP/app.js
+++ NODE-FIRST-APP/app.js
@@ -6,6 +6,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./Routes/admin');
 
 const shopRouter = require('./Routes/shop');
