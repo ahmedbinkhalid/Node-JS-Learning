a087d816c4420835d509211da101aad002dc00c2
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-07-28 17:12:10 +0500

    Adding a product model

--- NODE-FIRST-APP/controllers/products.js
+++ NODE-FIRST-APP/controllers/products.js
@@ -1,15 +1,17 @@
-const products = [];
+const Product = require('../models/products');
 
 exports.getAddProducts = (req, res, next)=>{
     res.render('add-product', {pageTitle : 'Add Product', path: '/admin/add-product'});
 };
 
 exports.postAddProduct = (req, res, next)=>{
-    products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts= (req, res, next)=>{
+    const products = Product.fetchAll();
     res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
     
 };
