3e27831bd93714353cd15d7aa4d2c0599af02fb3
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-09-07 20:22:47 +0500

    Creating the Shop structure

--- NODE-FIRST-APP/controllers/products.js
+++ NODE-FIRST-APP/controllers/products.js
@@ -1,7 +1,7 @@
 const Product = require('../models/products');
 
 exports.getAddProducts = (req, res, next)=>{
-    res.render('add-product', {pageTitle : 'Add Product', path: '/admin/add-product'});
+    res.render('admin/add-product', {pageTitle : 'Add Product', path: '/admin/add-product'});
 };
 
 exports.postAddProduct = (req, res, next)=>{
@@ -12,7 +12,7 @@ exports.postAddProduct = (req, res, next)=>{
 
 exports.getProducts= (req, res, next)=>{
     Product.fetchAll(products =>{
-        res.render('shop', {prods: products, pageTitle: 'Shop', path: '/'});
+        res.render('shop/product-list', {prods: products, pageTitle: 'Shop', path: '/'});
     });
     
 };
