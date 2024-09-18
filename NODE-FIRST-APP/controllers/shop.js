de42a634ee8b932c9c5080f9de9c4212975ae1f7
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2024-09-18 23:23:11 +0500

    cart model

--- NODE-FIRST-APP/controllers/shop.js
+++ NODE-FIRST-APP/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/products');
+const Cart = require('../models/cart');
 
 exports.getProducts= (req, res, next)=>{
     Product.fetchAll(products =>{
@@ -29,7 +30,9 @@ exports.getcart = (req, res, next) =>{
 }
 exports.postCart = (req, res, next) =>{
     const prodId = req.body.productId;
-    console.log(prodId);
+    Product.FindById(prodId, (product) =>{
+        Cart.addProduct(prodId, product.price);
+    })
     res.redirect('/cart');
 }
 exports.getOrders = (req, res, next) =>{
