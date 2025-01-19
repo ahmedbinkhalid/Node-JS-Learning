e4d47e5821005a1c1cd6f60f9240a0a61b931787
Ahmed Bin Khalid <101318581+ahmedbinkhalid@users.noreply.github.com>
2025-01-19 23:41:35 +0500

    Working with mySql (connected to database)

--- NODE-FIRST-APP/controllers/error.js
+++ NODE-FIRST-APP/controllers/error.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
-exports.get404 = (req, res, next)=>{
-    res.status(404).render('404', {pageTitle: 'Page not found', path: '/404'});
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
